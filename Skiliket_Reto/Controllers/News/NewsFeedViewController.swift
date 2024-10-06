75b08f905ebf3b879a3c85df63eeb2cc6d395a39
Carlos Paez <carlosalbertopaez2002@gmail.com>
2024-10-05 21:08:13 -0600

    Add news feed section

---
+++ Skiliket_Reto/Controllers/News/NewsFeedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewsFeedViewController.swift
+//  Skiliket_Reto
+//
+//  Created by Carlos Alberto Paez De la Cruz on 05/10/24.
+//
+
+import UIKit
+
+class NewsFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Skiliket_Reto/Models/NewsArticle.swift
@@ -0,0 +1,8 @@
+//
+//  NewsArticle.swift
+//  Skiliket_Reto
+//
+//  Created by Carlos Alberto Paez De la Cruz on 05/10/24.
+//
+
+import Foundation
