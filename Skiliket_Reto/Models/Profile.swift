6f38fde9934ae00838bf80612908da5cbc1d6e8e
Pedro Luis Pérez Collado <plpcollado93@gmail.com>
2024-10-08 16:14:02 -0600

    Filling model profile

--- Skiliket_Reto/Models/Profile.swift
+++ Skiliket_Reto/Models/Profile.swift
@@ -5,4 +5,67 @@
 //  Created by Pedro Luis Pérez Collado on 07/10/24.
 //
 
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let profile = try? JSONDecoder().decode(Profile.self, from: jsonData)
+
 import Foundation
+
+// MARK: - Profile
+class Profile: Codable {
+    let profile: ProfileClass
+
+    init(profile: ProfileClass) {
+        self.profile = profile
+    }
+}
+
+// MARK: - ProfileClass
+class ProfileClass: Codable {
+    let username, userID: String
+    let contactInfo: ContactInfo
+    let stats: Stats
+
+    enum CodingKeys: String, CodingKey {
+        case username
+        case userID = "user_id"
+        case contactInfo = "contact_info"
+        case stats
+    }
+
+    init(username: String, userID: String, contactInfo: ContactInfo, stats: Stats) {
+        self.username = username
+        self.userID = userID
+        self.contactInfo = contactInfo
+        self.stats = stats
+    }
+}
+
+// MARK: - ContactInfo
+class ContactInfo: Codable {
+    let title, email, phone, address: String
+
+    init(title: String, email: String, phone: String, address: String) {
+        self.title = title
+        self.email = email
+        self.phone = phone
+        self.address = address
+    }
+}
+
+// MARK: - Stats
+class Stats: Codable {
+    let enrolledProjects, publishedNews: Int
+
+    enum CodingKeys: String, CodingKey {
+        case enrolledProjects = "enrolled_projects"
+        case publishedNews = "published_news"
+    }
+
+    init(enrolledProjects: Int, publishedNews: Int) {
+        self.enrolledProjects = enrolledProjects
+        self.publishedNews = publishedNews
+    }
+}
+
