c28389695789fce92ff0ffd25b471bbf9e4ce7fc
Pedro Luis Pérez Collado <plpcollado93@gmail.com>
2024-10-08 16:01:24 -0600

    Adding structure for profile view

---
+++ Skiliket_Reto/Controllers/Profile/ProfileViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewController.swift
+//  Skiliket_Reto
+//
+//  Created by Pedro Luis Pérez Collado on 08/10/24.
+//
+
+import Foundation
---
+++ Skiliket_Reto/Models/Profile.swift
@@ -0,0 +1,8 @@
+//
+//  Profile.swift
+//  Skiliket_Reto
+//
+//  Created by Pedro Luis Pérez Collado on 07/10/24.
+//
+
+import Foundation
