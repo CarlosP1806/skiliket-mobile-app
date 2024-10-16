98196e18c6a0ea40a6479ac3b8ad0789e8aa6914
Antonio Pacheco <antonio.pacheco.ch@gmail.com>
2024-10-16 03:43:10 -0600

    Add Sound Graph Design

--- Skiliket_Reto/Views/SoundLineChartUIView.swift
+++ Skiliket_Reto/Views/SoundLineChartUIView.swift
@@ -8,63 +8,148 @@
 import SwiftUI
 import Charts
 
+// Extensión para redondear esquinas específicas
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(
+            roundedRect: rect,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius)
+        )
+        return Path(path.cgPath)
+    }
+}
+
 struct SoundLineChartUIView: View {
     var soundData: [Sound]
-    @State private var selectedReading: Sound?
     
     var body: some View {
-        VStack{
-            Text("Sound")
-                .font(.title)
-                .padding()
+        VStack {
+            Spacer().frame(height: 30)
+            Text("Noise Measurements")
+                .font(.title3)
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity, alignment: .center)
+            Spacer()
             
-            Chart {
-                ForEach(soundData) { reading in
-                    LineMark(
-                        x: .value("Time", reading.timeStamp),
-                        y: .value("Sound", Double(reading.value) ?? 0.0)
-                    )
-                    .interpolationMethod(.catmullRom)
-                }
-            }
-            .chartYScale(domain: 0...100)
-            .chartXAxisLabel("Date&time", position: .bottom)
-            .chartYAxisLabel("°Celsius", position: .leading)
-            .padding()
-            .chartOverlay { proxy in
-                GeometryReader { geo in
-                    Rectangle().fill(Color.clear).contentShape(Rectangle())
-                        .gesture(
-                            DragGesture()
-                                .onChanged { value in
-                                    let location = value.location
-                                    if let date: Date = proxy.value(atX: location.x),
-                                       let closestReading = soundData.min(by: {
-                                           abs($0.timeStamp.timeIntervalSince(date)) <
-                                           abs($1.timeStamp.timeIntervalSince(date))
-                                       }) {
-                                        selectedReading = closestReading
-                                    }
-                                }
-                        )
-                }
-            }
+            ZStack {
+                GeometryReader { geometry in
+                    VStack(spacing: 0) {
+                        let width = geometry.size.width * 0.8
+                        let heightRed = CGFloat(geometry.size.height) * 0.305
+                        let heightYellow = CGFloat(geometry.size.height) * 0.305
+                        let heightGreen = CGFloat(geometry.size.height) * 0.30
                         
-            if let selectedReading = selectedReading {
-                Text("Sound: \(selectedReading.value) °C")
-                    .font(.headline)
-                Text("Date: \(selectedReading.timeStamp.formatted(date: .numeric, time: .shortened))")
-                    .font(.subheadline)
-                    .padding(.top, 4)
+                        Color.red
+                            .frame(width: width, height: heightRed)
+                            .cornerRadius(10, corners: [.topLeft])
+                            .offset(x: geometry.size.width * 0.2)
+                        
+                        Color.yellow
+                            .frame(width: width, height: heightYellow)
+                            .offset(x: geometry.size.width * 0.2)
+                        
+                        Color.green
+                            .frame(width: width, height: heightGreen)
+                            .cornerRadius(10, corners: [.bottomLeft])
+                            .offset(x: geometry.size.width * 0.2)
+                    }
+                }
+                
+                .overlay(
+                    Chart {
+                        ForEach(soundData) { data in
+                            LineMark(
+                                x: .value("Time", data.timeStamp),
+                                y: .value("Sound", Double(data.value) ?? 0.0)
+                            )
+                            .foregroundStyle(.white)
+                        }
+                        
+                        RuleMark(y: .value("Low Level", 50))
+                            .lineStyle(StrokeStyle(lineWidth: 1, dash: [5]))
+                            .foregroundStyle(.green)
+                        
+                        RuleMark(y: .value("Moderate Level", 100))
+                            .lineStyle(StrokeStyle(lineWidth: 1, dash: [5]))
+                            .foregroundStyle(.yellow)
+                        
+                        RuleMark(y: .value("High Level", 150))
+                            .lineStyle(StrokeStyle(lineWidth: 1, dash: [5]))
+                            .foregroundStyle(.red)
+                    }
+                    .chartYScale(domain: 0...150)
+                    .chartYAxis {
+                        AxisMarks(position: .leading) { value in
+                            if let yValue = value.as(Double.self) {
+                                if yValue == 150 {
+                                    AxisValueLabel { Text("150 dB") }
+                                        .foregroundStyle(.white)
+                                        .offset(y: 4)
+                                } else if yValue == 50 || yValue == 100 {
+                                    AxisValueLabel { Text("\(Int(yValue)) dB") }
+                                        .foregroundStyle(.white)
+                                } else {
+                                    AxisValueLabel().foregroundStyle(.white)
+                                }
+                            }
+                        }
+                    }
+                    .chartXAxis {
+                        AxisMarks(position: .bottom) { AxisValueLabel().foregroundStyle(.white) }
+                    }
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.green, lineWidth: 1)
+                            .padding(.top, -10)
+                    )
+                )
             }
+            .padding()
+            .frame(maxHeight: 300)
+            
+            Spacer().frame(height: 20)
+            
+            VStack {
+                Text("Current Noise Measurement")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                HStack {
+                    Image(systemName: "speaker.wave.2.fill")
+                        .resizable()
+                        .frame(width: 50, height: 40)
+                        .foregroundColor(.green)
+                        .padding(.trailing, 20)
+                    Text("\(soundData.last?.value ?? "0") dB")
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundColor(.white)
+                }
+                .frame(maxWidth: .infinity, alignment: .center)
+            }
+            .padding()
+            .padding(.bottom, 250)
         }
+        .background(Color.black)
     }
 }
 
 #Preview {
     SoundLineChartUIView(soundData: [
-        Sound(value: "22.0", timeStamp: Date().addingTimeInterval(-3600)),
-        Sound(value: "23.5", timeStamp: Date().addingTimeInterval(-1800)),
-        Sound(value: "24.0", timeStamp: Date())
+        Sound(value: "50", timeStamp: Date().addingTimeInterval(-3600 * 4)),
+        Sound(value: "100", timeStamp: Date().addingTimeInterval(-3600 * 3)),
+        Sound(value: "120", timeStamp: Date().addingTimeInterval(-3600 * 2)),
+        Sound(value: "80", timeStamp: Date().addingTimeInterval(-3600)),
+        Sound(value: "150", timeStamp: Date())
     ])
 }
